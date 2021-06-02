5bfcc098e44fc499a20054755e7136fddb37b49b
Neil Kleynhans <ntkleynhans@gmail.com>
2021-06-02 16:40:18 +0200

    complete execute change set

--- scripts/cf_execute_change_set.py
+++ scripts/cf_execute_change_set.py
@@ -7,7 +7,7 @@ from yaml import dump, Dumper
 def _wait(change_set_name, stack_name, retry=3, wait_time=15):
     cf = boto3.client('cloudformation', region_name='us-east-2')
     print("...waiting for change set create complete...")
-    for x in range(3):
+    for x in range(retry):
         response = cf.describe_change_set(ChangeSetName=change_set_name, StackName=stack_name)
         if response['Status'] != 'CREATE_COMPLETE':
             time.sleep(wait_time)
