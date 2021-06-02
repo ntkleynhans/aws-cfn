e5dca1df8650c1a2c0bbc1a97cd92bf04638b977
Neil Kleynhans <ntkleynhans@gmail.com>
2021-06-02 16:40:09 +0200

    complete execute change set

--- scripts/cf_execute_change_set.py
+++ scripts/cf_execute_change_set.py
@@ -1,25 +1,36 @@
 import sys
-
+import time
 import boto3
-
 from yaml import dump, Dumper
 
 
-def main(stack_name, change_set_name):
+def _wait(change_set_name, stack_name, retry=3, wait_time=15):
     cf = boto3.client('cloudformation', region_name='us-east-2')
-    response = cf.describe_change_set(ChangeSetName=change_set_name, StackName=stack_name)
-    print(dump(response, Dumper=Dumper))
+    print("...waiting for change set create complete...")
+    for x in range(3):
+        response = cf.describe_change_set(ChangeSetName=change_set_name, StackName=stack_name)
+        if response['Status'] != 'CREATE_COMPLETE':
+            time.sleep(wait_time)
+        else:
+            print(dump(response, Dumper=Dumper))
+            return
+    print(dump(response, Dumper=Dumper))    
+    raise ValueError()
 
-    """
+
+def main(stack_name, change_set_name):
+    _wait(stack_name, change_set_name)
+
+    cf = boto3.client('cloudformation', region_name='us-east-2')
     response = cf.execute_change_set(
-        ChangeSetName='update-with-elb',
+        ChangeSetName=change_set_name,
         StackName=stack_name,
     )
     print(dump(response, Dumper=Dumper))
     waiter = cf.get_waiter('stack_update_complete')
     print("...waiting for stack to update...")
     waiter.wait(StackName=stack_name)
-    """
+
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
